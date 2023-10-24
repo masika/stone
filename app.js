5272b827228a31c739ff7de974c64c04c5219abc
Matsumoto Kaoru <info@tightrope.jp>
2023-10-25 01:43:29 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let record_btn=document.getElementById("record_btn");
+let count=0
+let url='./control.php'
+
+record_btn.addEventListener('click',function(){
+  count++
+  console.log(JSON.stringify(count));
+  fetch(url , {
+    method: "POST",
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(count)
+  }).then((response) => {
+    if(!response.ok) {
+        console.log('error!');
+    } 
+    //console.log('ok!',response.json());
+    return response.json();
+  }).then((data)  => {
+      console.log(data);
+  }).catch((error) => {
+      console.log(error);
+  });
+})
+
+
+
+
