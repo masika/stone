74ef9093d85d38d6f522d4d218439c7323c7d6bd
Matsumoto Kaoru <info@tightrope.jp>
2023-10-25 23:55:22 +0900

    ストリームテスト

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
 let record_btn=document.getElementById("record_btn");
-let count=0
+let message_area=document.getElementById("message_area");
 let url='https://tightrope.jp/stone/server/control.php'
 
 record_btn.addEventListener('click',function(){
-  count++
-  console.log(JSON.stringify(count));
+  calcRPM()
+})
+
+let timeArray=[];
+let averageCount=2;
+let prevTime=0
+
+function calcRPM(){
+  let now=Math.floor(performance.now())
+  
+  if(timeArray.length > averageCount){
+    timeArray.shift();
+  }
+  
+  if(prevTime==0){
+    console.log("一回目のクリック")
+  }else{
+    let span = now - prevTime
+    let rpm = (60*1000)/span
+    rpm=Math.floor((rpm*10))/10
+    timeArray.push(rpm)
+    console.log(timeArray)
+
+    post(rpm)
+  }
+  
+  prevTime=now
+}
+
+
+function post(message){
+  console.log(message);
+  message_area.innerText=message
   fetch(url , {
     method: "POST",
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(count)
+    //headers: { 'Content-Type': 'application/json' },
+    body: message
   }).then((response) => {
     if(!response.ok) {
         console.log('error!');
     } 
-    //console.log('ok!',response.json());
-    return response.json();
+    //console.log('ok!',response.text());
+    return response.text();
   }).then((data)  => {
       console.log(data);
   }).catch((error) => {
       console.log(error);
   });
-})
+}
 
 
 
--- read.js
+++ read.js
@@ -1,5 +1,16 @@
-	fetch('https://tightrope.jp/stone/server/current.txt')
-	.then(response => response.text())
-	.then(data => { 
-		console.log(data)
-	});
+const evtSource = new EventSource("https://tightrope.jp/stone/server/stream.php", {
+  withCredentials: true,
+});
+
+evtSource.addEventListener("ping", function (event) {
+	console.log(event)
+  const newElement = document.createElement("li");
+  const eventList = document.getElementById("list");
+  const time = JSON.parse(event.data).time;
+  newElement.textContent = "ping at " + time;
+  eventList.appendChild(newElement);
+});
+
+evtSource.onerror = function (err) {
+  console.error("EventSource failed:", err);
+};
