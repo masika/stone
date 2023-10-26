b7ecc857d44abb4e450708bb7cb98a5dffe7984d
Matsumoto Kaoru <info@tightrope.jp>
2023-10-26 16:36:27 +0900

    回転を再現

--- read.js
+++ read.js
@@ -1,16 +1,15 @@
-//setTimeout( function() {
-
-	
 const evtSource = new EventSource("https://tightrope.jp/stone/server/stream.php");
+let rpm_txt=document.getElementById("rpm_txt");
+let stone=document.getElementById("stone");
+let rpm=0
+let deg=0
 
-// evtSource.addEventListener("ping", function (event) {
-// 	console.log(event)
-//   const newElement = document.createElement("li");
-//   const eventList = document.getElementById("list");
-//   const time = JSON.parse(event.data).time;
-//   newElement.textContent = "ping at " + time;
-//   eventList.appendChild(newElement);
-// });
+evtSource.addEventListener("ping", function (event) {
+	//console.log(event)
+	const data=JSON.parse(event.data)
+	rpm=data.rpm
+	rpm_txt.innerText=rpm
+});
 
 evtSource.onerror = function (err) {
   console.error("EventSource failed:", err);
@@ -21,8 +20,11 @@ evtSource.onopen = function() {
 };
 
 
-evtSource.onmessage = function(e) {
-	console.log("onmessage");
-};
+setInterval(function(){
+	let rps = rpm / 60;
+	let r=rps/20
+	deg+=360 * r
+	deg=deg%360;
 
-//},1500);
+	stone.style.transform = "rotate(" + deg + "deg)";
+},1000/20)
